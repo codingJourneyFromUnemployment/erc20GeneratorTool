bbe2da4c1a6ae885b888e849102ced587a4a66e9
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-12-01 09:24:18 +0800

    getAllErc20 function

--- contracts/LookUpContract.sol
+++ contracts/LookUpContract.sol
@@ -6,21 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract LookUpContract is Ownable{
 
   struct ERC20token {
-    uint tokenID,
-    address owner,
-    string tokenSupply,
-    string tokenName,
-    string tokenSymbol,
-    string tokenAddress,
-    string tokenTransactionHash,
-    string tokenCreatedDate
+    uint tokenID;
+    address owner;
+    string tokenSupply;
+    string tokenName;
+    string tokenSymbol;
+    string tokenAddress;
+    string tokenTransactionHash;
+    string tokenCreatedDate;
     
   } 
 
   struct Donation {
-    uint256 donationID,
-    address donor,
-    uint256 fund, 
+    uint256 donationID;
+    address donor;
+    uint256 fund;
   }
 
   uint256 public listingFee = 0.025 ether;
@@ -35,6 +35,54 @@ contract LookUpContract is Ownable{
   constructor() Ownable(msg.sender) {
     tokenIndex = 0;
     donationIndex = 0;
+  }
+
+  function createERC20Token(
+    address _owner,
+    string memory _tokenSupply,
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    string memory _tokenAddress,
+    string memory _tokenTransactionHash,
+    string memory _tokenCreatedDate
+  )  external payable returns (
+    uint256,
+    address,
+    string memory,
+    string memory,
+    string memory,
+    string memory,
+    string memory,
+    string memory,
+  ) {
+    require(msg.value >= listingFee, "Insufficient listing fee");
+
+    tokenIndex++;
+    erc20Tokens[tokenIndex] = ERC20token(
+      tokenIndex,
+      _owner,
+      _tokenSupply,
+      _tokenName,
+      _tokenSymbol,
+      _tokenAddress,
+      _tokenTransactionHash,
+      _tokenCreatedDate
+    );
+
+    emit TokenCreated(tokenIndex, _owner, _tokenAddress);
+
+    return (
+      tokenIndex,
+      _owner,
+      _tokenSupply,
+      _tokenName,
+      _tokenSymbol,
+      _tokenAddress,
+    );
+  }
+
+  function getAllERC20Tokens() public view returns (ERC20token[] memory) {
+    
   }
 
 }
