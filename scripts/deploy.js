9abeba9f562de2d2a812d3d8e9a0f292ceb9ae35
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-12-01 23:01:16 +0800

    script done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,35 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const colors = require('colors');
+const hre = require('hardhat');
+const { promptForContractDeployment } = require('./helpers');
+
+require('dotenv').config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [contractName, argumentsArray] = await promptForContractDeployment();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  console.log(`Deploying ${contractName}`.cyan);
+  const signers = await hre.ethers.getSigners();
+  const deployer = signers[0];
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  if (hre.network.name === 'localhost') {
+    console.log('Deploying to local network'.cyan);
+  } else {
+    console.log(`Deploying to ${hre.network.name}`.cyan);
+  }
 
-  await lock.waitForDeployment();
+  const contractFactory = await hre.ethers.getContractFactory(contractName, deployer);
+  const contract = await contractFactory.deploy(...argumentsArray);
+  await contract.waitForDeployment();
+  const contractAddress = await contract.getAddress();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Deployed ${contractName} to ${contractAddress}`.green);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  }); 
